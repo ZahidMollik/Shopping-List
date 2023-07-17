913317a826f12743f3223fba415f5a0d9aa69cb2
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2023-07-17 17:22:28 +0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+         'poppin':['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
